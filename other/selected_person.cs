086af65b717e222a99cd7c93f7c72ce3794671f8
AMTitan <amtitan@icloud.com>
2020-12-15 20:49:50 -0500

    Select
    lets you select a person to be

---
+++ other/selected_person.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selected_person : MonoBehaviour
+{
+    public int selected;
+    
+    public void bow()
+    {
+        selected = 1;
+
+        PlayerPrefs.SetInt("Selcted", selected);
+        PlayerPrefs.Save();
+    }
+
+    public void Crossbow()
+    {
+        selected = 2;
+
+        PlayerPrefs.SetInt("Selcted", selected);
+        PlayerPrefs.Save();
+    }
+
+    public void dual()
+    {
+        selected = 3;
+
+        PlayerPrefs.SetInt("Selcted", selected);
+        PlayerPrefs.Save();
+    }
+
+    public void Sniper()
+    {
+        selected = 4;
+
+        PlayerPrefs.SetInt("Selcted", selected);
+        PlayerPrefs.Save();
+    }
+
+    public void Uzi()
+    {
+        selected = 5;
+
+        PlayerPrefs.SetInt("Selcted", selected);
+        PlayerPrefs.Save();
+    }
+}
