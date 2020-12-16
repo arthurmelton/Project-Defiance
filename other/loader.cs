40f6bee27152e639ea531b5610592d4310022def
AMTitan <amtitan@icloud.com>
2020-12-15 20:55:54 -0500

    Loader
    for loading

---
+++ other/loader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class loader
+{
+
+    public enum Scene
+    {
+        game,
+        loading_scene,
+        mainmenu,
+        people_select,
+        game_1,
+    }
+
+    private static Action onLoaderCallback;
+
+    public static void load(Scene scene)
+    {
+
+        onLoaderCallback = () =>
+        {
+            SceneManager.LoadScene(scene.ToString());
+        };
+
+        SceneManager.LoadScene(Scene.loading_scene.ToString());
+
+    }
+
+    public static void LoaderCallback()
+    {
+        if (onLoaderCallback != null)
+        {
+            onLoaderCallback();
+            onLoaderCallback = null;
+        }
+    }
+}
---
+++ other/loadingCallback.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadingCallback : MonoBehaviour
+{
+    private bool IsFirstUpdate = true;
+
+    private void Update()
+    {
+        if (IsFirstUpdate)
+        {
+            IsFirstUpdate = false;
+            loader.LoaderCallback();
+        }
+    }
+}
