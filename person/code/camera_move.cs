1bd7d7dc85e77ac1feb85f555c3ead7449de160d
AMTitan <amtitan@icloud.com>
2020-12-15 20:43:45 -0500

    Camera move
    moves the camera

---
+++ person/code/camera_move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move : MonoBehaviour
+{
+
+    public Rigidbody2D RB;
+
+    public GameObject GameObject;
+
+    void FixedUpdate()
+    {
+
+        RB.MovePosition(GameObject.transform.position);
+
+    }
+}
