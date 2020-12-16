ce8a71fb659054ffd6e1f307d45819d6d9b68958
AMTitan <amtitan@icloud.com>
2020-12-15 20:33:54 -0500

    Breakable Wall
    a wall that can break

---
+++ other/breakable_wall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakable_wall : MonoBehaviour
+{
+
+    public int health = 15;
+
+    private bullet Bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            Bullet = col.gameObject.GetComponent<bullet>();
+
+            health -= Bullet.dmg;
+        }
+
+    }
+}
