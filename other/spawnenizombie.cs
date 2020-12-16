39cc58c2ba13e894f11f97fa4f91d4a64b3eb0be
AMTitan <amtitan@icloud.com>
2020-12-15 20:49:05 -0500

    Spawn
    spawns the zombies

---
+++ other/spawnenizombie.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnenizombie : MonoBehaviour
+{
+    
+    public GameObject eni;
+    
+    public float nextActionTime = 0.0f;
+
+    public float timetillnextenispawn = 10f;
+
+    public float nextActionTime1 = 0.0f;
+
+    public float timetillnextactiontimeislowered = 10;
+
+    public Transform RB;
+
+    public time time;
+
+    public GameObject timetext;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = timetext.GetComponent<time>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextActionTime) 
+        {
+            nextActionTime = Time.time + timetillnextenispawn;
+
+            Instantiate(eni, RB.position, RB.rotation);
+
+            time.round += 1;
+        }
+
+        if(Time.time > nextActionTime1) 
+        {
+            nextActionTime1 = Time.time + timetillnextactiontimeislowered;
+
+            timetillnextenispawn -= 0.1f;
+        }
+    }
+}
