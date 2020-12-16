b97b965f48bc6fe2dfc46cbe1a204008ed3567ea
AMTitan <amtitan@icloud.com>
2020-12-15 20:28:50 -0500

    DestroyAfter
    Can destroy somthing after some time

---
+++ destroy_After.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy_After : MonoBehaviour
+{
+    public float Time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, Time);
+    }
+}
