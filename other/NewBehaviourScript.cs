cc8fdd6b3d3fa923a4ebb60f14cb5348caca9d26
AMTitan <amtitan@icloud.com>
2020-12-15 20:52:57 -0500

    Loader
    loads into the next level

---
+++ other/NewBehaviourScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int gamemode;
+
+    public int sellected;
+
+    public void playgame()
+    {
+        sellected = PlayerPrefs.GetInt("Selcted");
+        if(sellected != 0)
+        gamemode = PlayerPrefs.GetInt("gamemode");
+        if(gamemode == 1) 
+        {
+            loader.load(loader.Scene.game);
+        }
+
+        if(gamemode == 2) 
+        {
+            loader.load(loader.Scene.game_1);
+        }
+        
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+
+    }
+
+    public void deadreplay()
+    {
+        loader.load(loader.Scene.people_select);
+    }
+
+    public void mainmenu()
+    {
+        loader.load(loader.Scene.mainmenu);
+    }
+
+    public void select()
+    {
+        gamemode = 1;
+
+        PlayerPrefs.SetInt("gamemode", gamemode);
+        PlayerPrefs.Save();
+
+        loader.load(loader.Scene.people_select);
+    }
+
+    public void select1()
+    {
+        gamemode = 2;
+
+        PlayerPrefs.SetInt("gamemode", gamemode);
+        PlayerPrefs.Save();
+
+        loader.load(loader.Scene.people_select);
+    }
+
+}
