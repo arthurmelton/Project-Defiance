fba99715bdb105c247ba1f7f0fbf074dcf6eed13
AMTitan <amtitan@icloud.com>
2020-12-15 20:51:51 -0500

    Number of eni
    not finished but the number of eni

---
+++ other/numberofeni.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class numberofeni : MonoBehaviour
+{
+    public TextMesh tm;
+
+    public int numberofeni1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberofeni1 -= 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        tm.text = numberofeni1.ToString();
+    }
+}
