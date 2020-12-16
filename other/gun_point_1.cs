f27b624c965d704a85a2d583077cc43eeb244415
AMTitan <amtitan@icloud.com>
2020-12-15 20:36:05 -0500

    Gun Point
    Where the bullets are spawed

---
+++ other/gun_point_1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun_point_1 : MonoBehaviour
+{
+
+    public int sellected;
+    public Transform tran;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sellected = PlayerPrefs.GetInt("Selcted");
+
+        if(sellected == 1)
+        {
+            tran.position = new Vector3(0f, 0.5f);
+        }
+
+        if (sellected == 2)
+        {
+            tran.position = new Vector3(0f, 0.5f);
+        }
+
+        if (sellected == 4)
+        {
+            tran.position = new Vector3(0.05f, 0.53f);
+        }
+
+        if (sellected == 5)
+        {
+            tran.position = new Vector3(0f, 0.5f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
