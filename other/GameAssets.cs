9b710b71c1f9bbbbf95db4b4ab2b776c2f1395db
AMTitan <amtitan@icloud.com>
2020-12-15 20:35:33 -0500

    idk
    I dont rememeber

---
+++ other/GameAssets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class GameAssets : MonoBehaviour
+{
+    private static GameAssets _i;
+
+    public static GameAssets i
+    {
+        get
+        {
+            if (_i = null) _i = Instantiate(Resources.Load<GameAssets>("GameAssets"));
+            return _i;
+        }
+    }
+
+    public Transform pfDamagePopup;
+}
