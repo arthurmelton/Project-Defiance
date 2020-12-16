8b927ba1549ded23cafb790a2df89c99125d0a8f
AMTitan <amtitan@icloud.com>
2020-12-15 20:39:41 -0500

    To make sounds
    not working

---
+++ sounds/adioManiger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class adioManiger : MonoBehaviour
+{
+    public sound[] sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ sounds/sound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class sound 
+{
+
+    public string name;
+    
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+}
