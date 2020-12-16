8ffde5341a1f1fd177a2fbadd29e966bc1f3ce99
AMTitan <amtitan@icloud.com>
2020-12-16 11:57:11 -0500

    Sniper
    Snipers can now see 2x the distance of a normal person

---
+++ ifSniper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifSniper : MonoBehaviour
+{
+
+    public player player;
+    private float newSize;
+    public float sizeChangeAmount = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newSize = gameObject.GetComponent<Camera>().orthographicSize * sizeChangeAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.selected == 4)
+        {
+            gameObject.GetComponent<Camera>().orthographicSize = newSize;
+        }
+    }
+}
