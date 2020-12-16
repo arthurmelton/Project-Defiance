219fb4f8ffcf040055d88c8e9b8ad06eeff7c3f2
AMTitan <amtitan@icloud.com>
2020-12-15 20:30:57 -0500

    Money
    The beta money stuff

---
+++ money/Money.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+
+    public int Dollars;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    void Update() {
+
+
+    }
+    void OnTriggerEnter2D( Collider2D collision ) {
+
+        if( collision.gameObject.tag == "Money" ) {
+
+            Dollars++;
+            Destroy( collision.gameObject );
+
+        }
+    }
+}
