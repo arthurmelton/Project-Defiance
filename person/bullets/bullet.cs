22cbee8ae9b7a22b98975bad7b6af63a82fa9e52
AMTitan <amtitan@icloud.com>
2020-12-15 20:44:23 -0500

    Bullets
    Makes the bullets work and the bullets it self

---
+++ person/bullets/bullet.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+
+    public Rigidbody2D RB;
+
+    public int dmg = 3;
+
+    public int hardhit = 0;
+
+    public int selected;
+
+    private breakable_wall breakable_Wall;
+
+    public int gamemode;
+
+    private health health;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB.velocity = transform.right * speed;
+
+        gamemode = PlayerPrefs.GetInt("gamemode");
+
+        selected = PlayerPrefs.GetInt("Selcted");
+
+        if(selected == 1)
+        {
+            dmg = 10;
+        }
+
+        if (selected == 2)
+        {
+            dmg = 7;
+        }
+
+        if (selected == 4)
+        {
+            dmg = 15;
+        }
+
+        if (selected == 5)
+        {
+            dmg = 2;
+        }
+
+        hardhit = 0;
+
+    }
+
+    void Update()
+    {
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        //Debug.Log(col);
+        //Debug.Log(col.gameObject.name);
+        if (gameObject.tag == "Bullet" && col.gameObject.tag != "Bullet" && col.gameObject.tag != "Player" && col.gameObject.tag != "EniBullet" && col.gameObject.tag != "healthPod")
+        {
+            if (col.gameObject.tag == "destrutable_wall")
+            {
+                breakable_Wall = col.gameObject.GetComponent<breakable_wall>();
+
+                breakable_Wall.health -= dmg;
+            }
+
+            if (gameObject.tag == "Bullet" && col.gameObject.tag == "Eni" || col.gameObject.tag == "Zombie")
+            {
+                eni Eni = col.gameObject.GetComponent<eni>();
+                // decrese my health by the bullet damage
+                Eni.health -= dmg;
+            }
+
+            Destroy(gameObject);
+        }
+
+    }
+    
+
+}
---
+++ person/bullets/bullet1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet1 : MonoBehaviour
+{
+
+    public float speed = 20f;
+
+    public Rigidbody2D RB;
+
+    public int dmg = 3;
+
+    private health Health;
+
+    public float timetilldealeated = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB.velocity = transform.right * speed;
+
+        timetilldealeated = Time.time + timetilldealeated;
+    }
+
+    void Update()
+    {
+        if(Time.time > timetilldealeated) 
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log(col);
+        //Debug.Log(col.gameObject.name);
+        if (gameObject.tag == "EniBullet" && col.gameObject.tag != "EniBullet" && col.gameObject.tag != "Eni" && col.gameObject.tag != "Bullet" && col.gameObject.tag != "healthPod")
+        {
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.tag == "Player")
+        {
+            Health = col.gameObject.GetComponent<health>();
+            Health.Health -= dmg;
+        }
+
+    }
+    
+
+}
